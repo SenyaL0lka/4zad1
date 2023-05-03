56284105d36142d30e10461b768ca7a4d26bde2e
SenyaL0lka <132232641+SenyaL0lka@users.noreply.github.com>
2023-05-03 14:53:09 +0300

    Initial commit

---
+++ 4zad1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 684; // число, для якого ми хочемо отримати розклад
+            Console.WriteLine($"Розклад числа {n} на прості множники:");
+
+            for (int i = 2; i <= n; i++)
+            {
+                int count = 0; // лічильник кількості входжень простого множника
+
+                while (n % i == 0)
+                {
+                    count++;
+                    n /= i;
+                }
+
+                if (count > 0)
+                {
+                    Console.WriteLine($"{i}^{count}");
+                }
+            }
+        }
+    }
+ }
